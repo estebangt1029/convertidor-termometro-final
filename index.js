a9e5be724efddf10a7e7e674bb856c11df33208e
esteban <1029estebangt@gmail.com>
2022-09-22 09:57:34 -0500

    agregue el boton reiniciar

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const boton=document.getElementById('boton')
-const celcius=document.getElementById('celcius') 
 const temperatura=document.getElementById('temperatura')
-
+const reiniciarb=document.getElementById('reiniciar')
 
 function convertir(){
     let gradosf=document.getElementById('farenheit').value
     let gradosc=(gradosf-32)*5/9
-    celcius.innerHTML=""+gradosc.toFixed(1)
+    let result= gradosc.toFixed(1)
+    document.getElementById('celcius').value=result
     if(gradosc>0){
         temperatura.innerHTML=""+gradosc.toFixed(1)
         temperatura.style.height=gradosc+'%'
@@ -15,5 +15,15 @@ function convertir(){
         temperatura.style.height=0+'%'
     }
 }
+function reiniciar(){
+    document.getElementById('farenheit').value=''
+    document.getElementById('celcius').value=''
+    temperatura.style.height=0
+}
+reiniciarb.addEventListener('click',reiniciar)
 boton.addEventListener('click',convertir)
+
+
+
+
 
